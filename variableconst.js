d7d209fb882fa948fa907b1d167dcceaa7e4968b
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-01-27 22:37:11 +0530

    Add files via upload

---
+++ variableconst.js
@@ -0,0 +1,13 @@
+const accountid=2024
+let accountemail="priya@gmail.com"
+var accountpassword="2023"
+accouncity="pune";
+let accountstate;
+
+// accountid=5 //not allowed
+accountemail="Pa@gmail.com"
+accountpassword=24
+accountcity="jalna"
+console.log(accountid);
+
+console.table([accountid,accountemail, accountpassword, accountcity,accountstate])
