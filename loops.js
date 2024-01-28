b66ad10eea77f9fc04f5bc16b35935207aade7e0
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-01-28 20:41:32 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,36 @@
+// Loops
+
+for(let count=1; count<=5;count++){
+    console.log("Hey DK!!!");
+}
+
+// calculate sum 1 to  5
+
+let sum = 0;
+
+for (let i=0;i<=5;i++){
+    sum = sum + 1;
+}
+console.log("sum = ", sum);
+console.log("Loop has ended");
+
+
+// calculate sum 1 to  n
+
+let sum = 0;
+let n = 5;
+
+for(let i=0;i<=n;i++){
+    sum = sum + i;
+
+}
+
+console.log("sum =", sum);
+console.log("Loop has ended");
+
+
+//print 1 to 5
+
+for(let i=0;i>=0;i++){
+    console.log("i =", i);
+}
