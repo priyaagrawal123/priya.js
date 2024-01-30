9b332974758d1620a5cf49b33812ad37b965b8fe
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-01-30 23:43:01 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+// alert=(3+3); //it will give error bcoz this is node not browser,it will give right output in browser
+
+let name="priya"
+let age=21
+let isloggedin=true
+let bankaccount=456789986666
+let temperature=null; //this means that this is not found
+let state;
+let atmosphere=0; //this means that atmosphere is zero
+console.table([name,age,isloggedin,bankaccount,tempreature,state,atmosphere]);
+
+//number=> 2 to power 53
+//bigint =>
+//symbol=>used for uniqueness 
+//
---
+++ datatypes2.js
@@ -0,0 +1,16 @@
+// non primitibe datatypes
+
+const heroes=["shaktiman","doga"]; //arrays
+console.log(heroes)
+
+let myheroes={
+    name:"Doga",
+    age:2211,
+}
+console.log(myheroes)  //objects
+
+const myfunction=function(){
+    console.log("hey priya"); //functions
+}
+console.log(myfunction)
+console.log(typeof(myfunction))
